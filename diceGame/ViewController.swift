4990df3e1df43a4704830a27d6cdc2cbeaa531a8
water <mywater888@gmail.com>
2021-08-01 12:56:36 +0800

    Initial Commit

---
+++ diceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceGame
+//
+//  Created by water on 2021/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
